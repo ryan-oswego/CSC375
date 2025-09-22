8a9d492cf98b509a0bb3dc564ff81ed28dcfbd6f
ryan-oswego <rmarchew@oswego.edu>
2025-09-22 18:12:31 -0400

    update

--- proj1/src/main/java/com/rmarchew/Main.java
+++ proj1/src/main/java/com/rmarchew/Main.java
@@ -1,7 +1,27 @@
 package com.rmarchew;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a simple root container
+        StackPane root = new StackPane();
+        
+        // Create a scene with the root container
+        Scene scene = new Scene(root, 800, 600);
+        
+        // Set up the stage (window)
+        primaryStage.setTitle("CSC375 Project");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
